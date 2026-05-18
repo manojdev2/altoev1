cbecab1c55989dcc31fd6f48b3da969ad3be4eec
manoj <69884509+manojdev2@users.noreply.github.com>
2026-05-18 14:17:49 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -3,7 +3,7 @@
 function App() {
   const [theme, setTheme] = useState(() => {
     try {
-      return localStorage.getItem("alto-theme") || "dark";
+      return localStorage.getItem("alto-theme") || "light";
     } catch { return "dark"; }
   });
 
